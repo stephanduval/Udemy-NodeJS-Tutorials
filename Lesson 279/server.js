d8707b9d28b3ef3fa6c20fc11ad56be802081881
Stephan DuVal <stephan.duval@gmail.com>
2022-07-12 10:54:48 -0400

    Lesson 286

--- Lesson 279/server.js
+++ Lesson 279/server.js
@@ -1,3 +1,8 @@
 const http = require("http");
+// import { http } frpom "http";
+
+const server = http.createServer(() => {
+  console.log("I hear you!  Thanks for the request");
+});
 
 server.listen(3000);
